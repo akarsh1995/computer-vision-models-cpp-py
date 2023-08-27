3be86eba7739516f801debd8d1531b4db4462f1a
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-08-27 21:42:39 +0530

    refactor(convlayer): upsampling should be downsampling instead

--- src_py/models/resnet/model.py
+++ src_py/models/resnet/model.py
@@ -13,6 +13,7 @@ class CommonBlock(nn.Module):
                 nn.Conv2d(output_channel, block_channel, kernel_size=1, stride=1),
                 nn.BatchNorm2d(block_channel),
                 nn.ReLU(),
+                # padding of 1 is needed to maintain the channel width and height shape
                 nn.Conv2d(
                     block_channel, block_channel, kernel_size=3, stride=1, padding=1
                 ),
@@ -39,7 +40,7 @@ class BlockGroup(nn.Module):
         **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
-        self.upsampling_layer = nn.Conv2d(
+        self.downsampling_layer = nn.Conv2d(
             input_channel,
             output_channel,
             kernel_size=3,
@@ -55,6 +56,10 @@ class BlockGroup(nn.Module):
                     block_channel,
                     block_channel,
                     kernel_size=3,
+                    # in the first block repetition stride of 1
+                    # because the channel dim is halved by maxpooling in the beginning.
+                    # whereas in rest of the block repetitions stride of 2 is required to
+                    # half the width and height of the channel
                     stride=first_layer_center_conv_stride,
                     padding=1,
                 ),
@@ -70,8 +75,8 @@ class BlockGroup(nn.Module):
             self.seq.append(CommonBlock(output_channel, block_channel))
 
     def forward(self, x):
-        # for every first block, upsampling needs to be done to propogate values to subsequent blocks
-        upsampled = self.upsampling_layer(x)
+        # for every first block, upsampling needs to be done to propogate values to the next block
+        upsampled = self.downsampling_layer(x)
         x = torch.relu(self.first_block(x) + upsampled)
         return self.seq(x)
 
@@ -80,14 +85,22 @@ class ResNet50(nn.Module):
     def __init__(self, repetitions: List[int], *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
+        # Not mentioned in the paper but padding of 3 and stride of 2 is required
+        # output_width = (input_width - kernel_width + 2 * padding) / stride + 1
+        # output_height = (input_height - kernel_height + 2 * padding) / stride + 1
+        # ((224 - 7 + 2 * 3) / 2) + 1
+        # 111 + 1 = 112 x 112
         self.conv = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+
+        # ((112 - 3 + 2 * 1) / 2) + 1
+        #  55 + 1 = 56 x 56
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.block_groups = nn.Sequential()
 
-        first_layer_center_conv_stride = 1
-        input_channel = 64
-        block_channel = 64
-        output_channel = block_channel * 4
+        first_layer_center_conv_stride = 1  # 1, 2, 2, 2
+        input_channel = 64  # 64, 256, 512, 1024
+        block_channel = 64  # 64, 128, 256, 512
+        output_channel = block_channel * 4  # 256, 512, 1024, 2048
 
         for r in repetitions:
             self.block_groups.append(
@@ -105,18 +118,12 @@ class ResNet50(nn.Module):
             block_channel = block_channel * 2  # 64 * 2
             output_channel = block_channel * 4  # 64 * 2 * 4
 
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(input_channel, 1000)
+
     def forward(self, x):
         x = self.maxpool(self.conv(x))
         x = self.block_groups(x)
-        return x
-
-
-def main():
-    x = torch.randn((1, 3, 224, 224))
-
-    r50 = ResNet50(repetitions=[3, 4, 6, 3])
-    print(r50(x).shape)
-
-
-if __name__ == "__main__":
-    main()
+        x = self.avgpool(x)
+        x = self.fc(x.flatten(1))
+        return nn.functional.softmax(x, dim=1)
