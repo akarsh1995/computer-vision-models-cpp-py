0dad1d35b5688d44ee2504492c3bd43b95001be3
Akarsh Jain <akarsh.1995.02@gmail.com>
2023-08-29 22:09:43 +0530

    update(yolo): batchnorm and relu ops added

--- src_py/models/yolo/model.py
+++ src_py/models/yolo/model.py
@@ -1,100 +1,86 @@
 from torch.functional import Tensor
-from torch.nn import BatchNorm2d, Conv2d, Linear, MaxPool2d
+from torch.nn import BatchNorm2d, Conv2d, Flatten, MaxPool2d
 import torch.nn as nn
 import torch
 
 
+class Conv2dBNReLU(nn.Module):
+    def __init__(self, *args, leaky=True, **kwargs) -> None:
+        super().__init__()
+        self.conv_bn_relu = nn.Sequential(
+            Conv2d(*args, **kwargs),
+            BatchNorm2d(args[1]),
+            nn.LeakyReLU(0.1) if leaky else nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.conv_bn_relu(x)
+
+
+class Yolo(nn.Module):
+    def __init__(
+        self,
+        n_cells_per_row: int,
+        n_bbox_predictors: int,
+        n_classes: int,
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self.n_classes = n_classes
+        self.n_bbox_predictors = n_bbox_predictors
+        self.n_cells_per_row = n_cells_per_row
+
+        self.layers = nn.Sequential(
+            Conv2dBNReLU(3, 64, kernel_size=7, stride=2, padding=3),
+            MaxPool2d(kernel_size=2, stride=2),
+            Conv2dBNReLU(64, 192, kernel_size=3, padding=1),
+            MaxPool2d(kernel_size=2, stride=2),  # stride=2, padding=1
+            Conv2dBNReLU(192, 128, kernel_size=1),
+            Conv2dBNReLU(128, 256, kernel_size=3, padding=1),
+            Conv2dBNReLU(256, 256, kernel_size=1),
+            Conv2dBNReLU(256, 512, kernel_size=3, padding=1),
+            MaxPool2d(kernel_size=2, stride=2),  # stride=2, padding=1
+            Conv2dBNReLU(512, 256, kernel_size=1),
+            Conv2dBNReLU(256, 512, kernel_size=3, padding=1),
+            Conv2dBNReLU(512, 256, kernel_size=1),
+            Conv2dBNReLU(256, 512, kernel_size=3, padding=1),
+            Conv2dBNReLU(512, 256, kernel_size=1),
+            Conv2dBNReLU(256, 512, kernel_size=3, padding=1),
+            Conv2dBNReLU(512, 256, kernel_size=1),
+            Conv2dBNReLU(256, 512, kernel_size=3, padding=1),
+            Conv2dBNReLU(512, 512, kernel_size=1),
+            Conv2dBNReLU(512, 1024, kernel_size=3, padding=1),
+            MaxPool2d(kernel_size=2, stride=2),  # stride=2, padding=1,
+            Conv2dBNReLU(1024, 512, kernel_size=1),
+            Conv2dBNReLU(512, 1024, kernel_size=3, padding=1),
+            Conv2dBNReLU(1024, 512, kernel_size=1),
+            Conv2dBNReLU(512, 1024, kernel_size=3, padding=1),
+            Conv2dBNReLU(1024, 1024, kernel_size=3, padding=1),
+            MaxPool2d(kernel_size=3, stride=2, padding=1),  # stride=2, padding=1,
+            Conv2dBNReLU(1024, 1024, kernel_size=3, padding=1),
+            Conv2dBNReLU(1024, 1024, kernel_size=3, padding=1, leaky=False),
+            Flatten(),
+            nn.Linear(n_cells_per_row * n_cells_per_row * 1024, 4096),
+            nn.Linear(
+                4096,
+                n_cells_per_row * n_cells_per_row * (n_classes + 5 * n_bbox_predictors),
+            ),
+        )
+
+    def forward(self, x):
+        return self.layers(x).view(
+            (
+                -1,
+                (self.n_classes + 5 * self.n_bbox_predictors),
+                self.n_cells_per_row,
+                self.n_cells_per_row,
+            )
+        )
+
+
 if __name__ == "__main__":
     input = torch.randn((1, 3, 448, 448))
-    conv = Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
-    x = conv(input)
-    print(x.shape)
-    maxpool = MaxPool2d(kernel_size=2, stride=2)
-    x = maxpool(x)
-    print(x.shape)
-    conv = Conv2d(64, 192, kernel_size=3, padding=1)
-    x = conv(x)
-    print(x.shape)
-    maxpool = MaxPool2d(kernel_size=2, stride=2)  # stride=2, padding=1
-    x = maxpool(x)
-    print(x.shape)
-
-    conv = Conv2d(192, 128, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(128, 256, kernel_size=3, padding=1)
-    x = conv(x)
-    conv = Conv2d(256, 256, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(256, 512, kernel_size=3, padding=1)
-    x = conv(x)
-    maxpool = MaxPool2d(kernel_size=2, stride=2)  # stride=2, padding=1
-    x = maxpool(x)
-    print(x.shape)
-
-    conv = Conv2d(512, 256, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(256, 512, kernel_size=3, padding=1)
-    x = conv(x)
-
-    conv = Conv2d(512, 256, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(256, 512, kernel_size=3, padding=1)
-    x = conv(x)
-
-    conv = Conv2d(512, 256, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(256, 512, kernel_size=3, padding=1)
-    x = conv(x)
-
-    conv = Conv2d(512, 256, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(256, 512, kernel_size=3, padding=1)
-    x = conv(x)
-
-    conv = Conv2d(512, 512, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(512, 1024, kernel_size=3, padding=1)
-    x = conv(x)
-    maxpool = MaxPool2d(kernel_size=2, stride=2)  # stride=2, padding=1
-    x = maxpool(x)
-    print(x.shape)
-
-    conv = Conv2d(1024, 512, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(512, 1024, kernel_size=3, padding=1)
-    x = conv(x)
-
-    conv = Conv2d(1024, 512, kernel_size=1)
-    x = conv(x)
-    conv = Conv2d(512, 1024, kernel_size=3, padding=1)
-    x = conv(x)
-
-    conv = Conv2d(1024, 1024, kernel_size=3, padding=1)
-    x = conv(x)
-
-    maxpool = MaxPool2d(kernel_size=3, stride=2, padding=1)  # stride=2, padding=1
-    x = maxpool(x)
-
-    print(x.shape)
-
-    conv = Conv2d(1024, 1024, kernel_size=3, padding=1)
-    x = conv(x)
-
-    conv = Conv2d(1024, 1024, kernel_size=3, padding=1)
-    x = conv(x)
-
-    print(x.shape)
-
-    flat = torch.nn.Flatten()
-
-    x = flat(x)
-    print(x.shape)
-
-    fc = nn.Linear(7 * 7 * 1024, 4096)
-    x = fc(x)
-    fc = nn.Linear(4096, 7 * 7 * 30)
-    x = fc(x)
-
-    x = x.reshape((30, 7, 7))
-
+    y = Yolo(7, 2, 20)
+    x = y(input)
     print(x.shape)
